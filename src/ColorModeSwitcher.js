37a81dbffe7f5edbd4d8c41570bc6a1fca8d84ec
Pooja R <pooja.ramesh@imaginelearning.com>
2023-02-01 17:27:42 +0530

    Toggle Button Corrections

--- src/ColorModeSwitcher.js
+++ src/ColorModeSwitcher.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import { useColorMode, useColorModeValue, IconButton } from '@chakra-ui/react';
-import { FaMoon, FaSun } from 'react-icons/fa';
+import { BiMoon, BiSun } from 'react-icons/bi';
 
 export const ColorModeSwitcher = props => {
   const { toggleColorMode } = useColorMode();
   const text = useColorModeValue('dark', 'light');
-  const SwitchIcon = useColorModeValue(FaMoon, FaSun);
+  const SwitchIcon = useColorModeValue(BiMoon, BiSun);
 
   return (
     <IconButton
       size="md"
       fontSize="lg"
       aria-label={`Switch to ${text} mode`}
+      title={`Switch to ${text} mode`}
       variant="ghost"
       color="current"
       marginLeft="2"
       onClick={toggleColorMode}
-      icon={<SwitchIcon />}
+      icon={<SwitchIcon size={25} />}
       {...props}
     />
   );
